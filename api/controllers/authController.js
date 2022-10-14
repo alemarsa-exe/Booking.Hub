28d74fd84dc09db906b913a9d3c66fcae5813cfa
alemarsa-exe <alex.mariacca@outlook.com>
2022-10-14 15:18:55 -0500

    HTTP status 403

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -81,7 +81,7 @@ export const loginUser = async (req, res, next) => {
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if (!isPasswordCorrect) return next(createError(400, "Wrong password or username"))
-        if(!user.status) return next(createError(400, "Please confirm your email"))
+        if(!user.status) return next(createError(403, "Please confirm your email"))
 
         const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT)
 
